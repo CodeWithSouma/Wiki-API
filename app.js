96e08077da6ee9de2d2174d1ad670ac786cd641c
Souma <souma.hitech@gmail.com>
2020-08-11 11:12:15 +0530

    add article/:articleTitle delete method

--- app.js
+++ app.js
@@ -112,6 +112,18 @@ app.route("/articles/:articleTitle")
             }
         }
     );
+})
+
+.delete(function(req,res){
+
+    Article.deleteOne({title:req.params.articleTitle},function(err){
+        if (!err) {
+            res.send("Succesfully deleted.");
+        } else {
+            res.send(err);
+        }
+    });
+
 });
 
 
